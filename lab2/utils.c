0cf42fe0e17c2f52d0fecff07693eabfccfbb088
Isabel Amaral <isabel.andre.amaral@gmail.com>
2022-03-18 16:59:53 +0000

    Corrected mask value

--- lab2/utils.c
+++ lab2/utils.c
@@ -20,7 +20,7 @@ int(util_get_MSB)(uint16_t val, uint8_t *msb) {
 int (util_sys_inb)(int port, uint8_t *value) {
   uint32_t* value_32 = (uint32_t*) malloc(sizeof(uint32_t));
   if (sys_inb(port, value_32) == 0) {
-      uint32_t mask = 0x00000011;
+      uint32_t mask = 0x000000ff;
       *value = (uint8_t) (mask & *value_32);
       return 0;
   }
