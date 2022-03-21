d308b6d76fb3c3b84b1338e99fe4307db45c579c
Hugo Castro <hreisc@gmail.com>
2022-03-21 11:29:46 +0000

    Update

--- lab2/timer.c
+++ lab2/timer.c
@@ -5,11 +5,39 @@
 
 #include "i8254.h"
 
+/* To be implemented by the students */
 int (timer_set_frequency)(uint8_t timer, uint32_t freq) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  //TODO: verificar freq valida
+  uint8_t* st = (uint8_t*) malloc(sizeof(uint8_t));
 
-  return 1;
+  if (timer_get_conf(timer, st) != 0) 
+    return 1;
+  uint8_t mask = 0x0f;
+  *st &= mask;
+  *st |= BIT(4) | BIT(5);
+
+  int timer_address;
+  switch(timer) {
+    case 0:
+      *st |= TIMER_SEL0;
+      timer_address = TIMER_0;
+      break;
+    case 1:
+      *st |= TIMER_SEL1;
+      timer_address = TIMER_1;
+      break;
+    case 2:
+      *st |= TIMER_SEL2;
+      timer_address = TIMER_2;
+      break;
+  }
+
+  uint16_t counter_value = (uint16_t) TIMER_FREQ/freq;
+  if (sys_outb(TIMER_CTRL, *st) != 0) 
+    return 1;
+  if (sys_outb(timer_address, counter_value) != 0) 
+    return 1;
+  return 0;
 }
 
 int (timer_subscribe_int)(uint8_t *bit_no) {
@@ -55,8 +83,7 @@ int (timer_get_conf)(uint8_t timer, uint8_t *st) {
   return 0;
 }
 
-int (timer_display_conf)(uint8_t timer, uint8_t st,
-                        enum timer_status_field field) {
+int (timer_display_conf)(uint8_t timer, uint8_t st, enum timer_status_field field) {
   union timer_status_field_val u;
 
   if (timer != 0 && timer != 1 && timer != 2)
