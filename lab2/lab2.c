01a1aac0212ae925f0fe00ffb29e9e4fe204287b
Milena Gouveia <milenaagouveia@gmail.com>
2022-03-24 11:37:59 +0000

    Implemented timer_test_read_config() and timer_test_time_base()

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -30,17 +30,30 @@ int main(int argc, char *argv[]) {
 }
 
 int(timer_test_read_config)(uint8_t timer, enum timer_status_field field) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  uint8_t* st = (uint8_t*) malloc(sizeof(uint8_t));
 
-  return 1;
+  if (timer_get_conf(timer, st) != 0){
+    printf ("Failed timer_get_conf\n");
+    return 1;
+  }
+
+  if (timer_display_conf(timer, *st, field) != 0){
+    printf ("Failed timer_display_conf\n");
+    return 1;
+  }
+
+  printf("Success\n");
+  return 0;
 }
 
 int(timer_test_time_base)(uint8_t timer, uint32_t freq) {
-  /* To be implemented by the students */
-  printf("%s is not yet implemented!\n", __func__);
+  if (timer_set_frequency(timer, freq) != 0){
+    printf("Failed timer_set_frequency\n");
+    return 1;
+  }
 
-  return 1;
+  printf("Success\n");
+  return 0;
 }
 
 int(timer_test_int)(uint8_t time) {
