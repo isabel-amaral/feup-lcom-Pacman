e11c8b087599e5b91abdc31aa3caa542dab82a2b
Isabel Amaral <isabel.andre.amaral@gmail.com>
2022-04-01 16:36:52 +0100

    Added lab3 files

---
+++ lab3/i8042.h
@@ -0,0 +1,6 @@
+#ifndef _LCOM_I8042_H_
+#define _LCOM_I8042_H_
+
+#include <lcom/lcf.h>
+
+#endif
---
+++ lab3/keyboard.c
@@ -0,0 +1,6 @@
+#include <lcom/lcf.h>
+#include <keyboard.h>
+
+#include <stdint.h>
+
+#include "i8042.h"
