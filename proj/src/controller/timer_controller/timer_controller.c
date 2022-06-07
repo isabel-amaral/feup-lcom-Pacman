73e914e917313e7c474e8c5481cd02d42506c3f5
Milena Gouveia <milenaagouveia@gmail.com>
2022-06-07 19:43:13 +0100

    Added variables to help time display

--- proj/src/controller/timer_controller/timer_controller.c
+++ proj/src/controller/timer_controller/timer_controller.c
@@ -2,46 +2,53 @@
 
 #include <stdint.h>
 
-float game_time_minutes;
-unsigned int game_time_seconds;
-unsigned int seconds_before_pause;
-float minutes_before_pause;
+unsigned int game_time;
+unsigned int timer_minutes;
+unsigned int timer_seconds;
+
+unsigned int time_before_pause;
+
 extern unsigned int int_counter;
 extern bool game_is_on;
 
+
 void initialize_game_time() {
-    game_time_minutes = 2;
-    game_time_seconds = 120;
+    game_time = 120;
+    update_timer_units();
 }
 
 void timer_interrupt_handler() {
     timer_int_handler();
-    if (int_counter%60 == 0)
+    if (int_counter % 60 == 0)
         decrease_game_time();
 }
 
 void decrease_game_time() {
-    game_time_seconds--;
-    game_time_minutes -= 0.01;
+    game_time--;
+    update_timer_units();
     check_time_out();
 }
 
 void pause_game() {
-    seconds_before_pause = game_time_seconds;
-    minutes_before_pause= game_time_minutes;
+    time_before_pause = game_time;
 }
 
 void continue_game() {
-    game_time_seconds = seconds_before_pause;
-    game_time_minutes = minutes_before_pause;
+    game_time = time_before_pause;
+    update_timer_units();
 }
 
 void increase_time() {
-    game_time_seconds += 30;
-    game_time_minutes += 0.30;
+    game_time += 30;
+    update_timer_units();
 }
 
 void check_time_out() {
-    if (game_time_seconds == 0) 
+    if (game_time == 0) 
         game_is_on = false;
+}
+
+void update_timer_units() {
+    timer_minutes = game_time / 60;
+    timer_seconds = game_time % 60;
 }
