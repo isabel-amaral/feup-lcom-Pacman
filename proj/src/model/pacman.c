0c24f2acdd694bc8897c9b7d4c2cb9a877d86c73
Sofia Moura <sofia.smoura@hotmail.com>
2022-06-08 16:57:20 +0100

    Fixed elements position

--- proj/src/model/pacman.c
+++ proj/src/model/pacman.c
@@ -5,6 +5,6 @@ Pacman pacman;
 void set_pacman_position() {
     pacman.pos.left_x = 500;
     pacman.pos.right_x = 525;
-    pacman.pos.top_y = 564;
-    pacman.pos.bottom_y = 589;
+    pacman.pos.top_y = 554;
+    pacman.pos.bottom_y = 579;
 }
