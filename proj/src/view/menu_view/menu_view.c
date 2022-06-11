8c0975743089895c50101d30ed91ce7a5fbdc0af
Milena Gouveia <milenaagouveia@gmail.com>
2022-06-11 18:46:59 +0100

    Added functions to draw win, game over and score

--- proj/src/view/menu_view/menu_view.c
+++ proj/src/view/menu_view/menu_view.c
@@ -11,11 +11,17 @@ extern uint8_t* pacman_text_pixmap;
 extern uint8_t* play_text_pixmap; 
 extern uint8_t* rules_text_pixmap; 
 extern uint8_t* exit_text_pixmap;
+extern uint8_t* win_text_pixmap; 
+extern uint8_t* score_text_pixmap; 
+extern uint8_t* game_over_text_pixmap;
 
 extern xpm_image_t pacman_text_info;
 extern xpm_image_t play_text_info;
 extern xpm_image_t rules_text_info;
 extern xpm_image_t exit_text_info;
+extern xpm_image_t win_text_info;
+extern xpm_image_t score_text_info;
+extern xpm_image_t game_over_text_info;
 
 void (draw_pacman_text)() {
     draw_xpm(pacman_text_pixmap, pacman_text_info, PACMAN_TEXT_X, PACMAN_TEXT_Y);
@@ -30,6 +36,18 @@ void (draw_play_text)() {
 }
 void (draw_exit_option)() {
     draw_xpm(exit_text_pixmap, exit_text_info, EXIT_TEXT_X, EXIT_TEXT_Y);
+}
+
+void (draw_win_text)() {
+    draw_xpm(win_text_pixmap, win_text_info, WIN_TEXT_X, WIN_TEXT_Y);
+}
+
+void (draw_game_over_text)() {
+    draw_xpm(game_over_text_pixmap, game_over_text_info, GAME_OVER_TEXT_X, GAME_OVER_TEXT_Y);
+}
+
+void (draw_score_text)() {
+    draw_xpm(score_text_pixmap, score_text_info, SCORE_TEXT_X, SCORE_TEXT_Y);
 }
 
 void (draw_menu)() {
--- proj/src/view/menu_view/menu_view.h
+++ proj/src/view/menu_view/menu_view.h
@@ -22,11 +22,22 @@
 #define EXIT_TEXT_WIDTH 240
 #define EXIT_TEXT_HEIGHT 80
 
+#define WIN_TEXT_X 401
+#define WIN_TEXT_Y 344
+
+#define GAME_OVER_TEXT_X 191
+#define GAME_OVER_TEXT_Y 344
+
+#define SCORE_TEXT_X 339
+#define SCORE_TEXT_Y 344
 
 void (draw_pacman_text)();
 void (draw_rules_text)();
 void (draw_play_text)();
 void (draw_exit_text)();
+void (draw_win_text)();
+void (draw_game_over_text)();
+void (draw_score_text)();
 void (erase_menu)();
 void (draw_menu)();
 
