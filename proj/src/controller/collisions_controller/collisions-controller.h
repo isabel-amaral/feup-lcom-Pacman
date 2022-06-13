12b22af6678c2028a07cf003a434e845bb449be6
Milena Gouveia <milenaagouveia@gmail.com>
2022-06-13 02:58:54 +0100

    Fixed collision functions

--- proj/src/controller/collisions_controller/collisions-controller.h
+++ proj/src/controller/collisions_controller/collisions-controller.h
@@ -6,20 +6,16 @@
 #include "../../model/ghosts_model/ghost.h"
 #include "../../model/pacman_model/pacman.h"
 
-bool (pacman_ghost_collision)(Position pacman_pos, Position ghost_pos);
 
-bool (pacman_maze_collision)(Position pacman_pos);
 
-bool (pacman_small_coin_collision)(Position pacman_pos);
+bool (pacman_ghost_collision)(Position ghost_pos);
 
-bool (pacman_big_coin_collision)(Position pacman_pos);
+bool (maze_collision)(Position position);
 
-bool (ghosts_collision)(Position ghost1_pos, Position ghost2_pos);
+bool (pacman_small_coin_collision)();
 
-bool (ghost_maze_collision)(Position ghost_pos);
+bool (pacman_big_coin_collision)();
 
-bool (ghost_small_coin_collision)(Position ghost_pos);
-
-bool (ghost_big_coin_collision)(Position ghost_pos);
+bool (ghosts_collision)();
 
 #endif
